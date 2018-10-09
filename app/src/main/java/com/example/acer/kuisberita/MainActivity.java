ce8674e67f3a1a9be313858dff5d2f3c90f2486e
stefenSitorus <antonius.stefen@gmail.com>
2018-10-09 16:13:30 +0700

    first commit

---
+++ app/src/main/java/com/example/acer/kuisberita/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.acer.kuisberita;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void selanjutnya1 (View view) {
+        Intent intent = new Intent( this, berita1.class);
+        startActivity(intent);
+    }
+    public void selanjutnya2 (View view){
+        Intent intent = new Intent ( this, berita2.class);
+        startActivity(intent);
+    }
+    public void selanjutnya3 (View view) {
+        Intent intent = new Intent(this, berita3.class);
+        startActivity(intent);
+    }
+}
